c58e21c1dfbba872a850a15b32eb577c3877dedd
jnphilipp <mail@jnphilipp.org>
2018-05-01 17:26:14 +0200

    Add profiles app.

--- objdetec/settings.py
+++ objdetec/settings.py
@@ -57,6 +57,7 @@ EMAIL_SUBJECT_PREFIX = '[objdetec] '
 # Application definition
 
 INSTALLED_APPS = [
+    'profiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
